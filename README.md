2ca7335a01ad0e249bff1fa4ef1d121df77bc77b
Peter Schweizer <peter.schweizer@gmail.com>
2023-12-14 22:07:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Golang + Templ + HTMLX
+
+## Small dummy app for testing those technologies in combination
+
+- Go HTTP Server
+- Templ templates
+- HTMLX
+- auto-rebuild via air (server is rebuild on file changes,
+  browser still needs to be manually reloaded via CTRL-r)
+
+## usage
+
+$ go install github.com/cosmtrek/air@latest
+$ go mod tidy
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	component := page("")
+	component.Render(r.Context(), w)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	username := r.Form.Get("username")
+	fmt.Println("got username: ", username)
+
+	component := hello(username)
+	component.Render(r.Context(), w)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getHandler(w, r)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("400 - only POST allowed!"))
+	})
+
+	mux.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			postHandler(w, r)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("400 - only POST allowed!"))
+	})
+
+	// Start the server.
+	fmt.Println("listening on http://localhost:8000")
+	if err := http.ListenAndServe("localhost:8000", mux); err != nil {
+		log.Printf("error listening: %v", err)
+	}
+}
